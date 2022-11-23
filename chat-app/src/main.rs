ae6c98b73e0ca21807d7314203cf568016327efc
Gotam Gorabh <gautamy672@gmail.com>
2022-11-24 01:01:03 +0530

    initial code

--- chat-app/src/main.rs
+++ chat-app/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+// Import all the rocket macros globally
+#[macro_use] extern crate rocket;
+
+#[get("/world")]   // get request to world path
+// handler function
+fn world() -> &'static str {
+     "Hello, world!"
 }
+
+#[launch]
+
+fn rocket() -> _ {
+      rocket::build().mount("/hello", routes![world]) // To create new rocket server instance and mount the route.
+}
+                
