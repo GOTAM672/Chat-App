f6b031a3a1b9ded05a71078a66205a7af7f82fd7
Gotam Gorabh <gautamy672@gmail.com>
2022-11-25 20:58:53 +0530

    structing message

--- chat-app/src/main.rs
+++ chat-app/src/main.rs
@@ -1,12 +1,30 @@
 // Import all the rocket macros globally
 #[macro_use] extern crate rocket;
 
+
 use rocket::tokio::sync::broadcast::{channel};
 
+
 #[get("/world")]   // get request to world path
+
+
 // handler function
 fn world() -> &'static str {
      "Hello, world!"
+}
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+
+
+struct Message{
+     /* Message has three fields room, username, message */
+ 
+     #[field(validate = len(..30))]
+     pub room: String,
+     #[field(validate = len(..20))]
+     pub username: String,
+     pub message: String,
 }
 
 #[launch]
