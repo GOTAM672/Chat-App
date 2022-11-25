3bcb6418e725916517b3b93a1a159221ae1cacca
Gotam Gorabh <gautamy672@gmail.com>
2022-11-25 20:47:19 +0530

    adding manage method

--- chat-app/src/main.rs
+++ chat-app/src/main.rs
@@ -1,6 +1,8 @@
 // Import all the rocket macros globally
 #[macro_use] extern crate rocket;
 
+use rocket::tokio::sync::broadcast::{channel};
+
 #[get("/world")]   // get request to world path
 // handler function
 fn world() -> &'static str {
@@ -10,6 +12,8 @@ fn world() -> &'static str {
 #[launch]
 
 fn rocket() -> _ {
-      rocket::build().mount("/hello", routes![world]) // To create new rocket server instance and mount the route.
+      rocket::build() 
+          .manage(channet::<Message>(1024).0)       // The manage method allows to add state to our rocket server instance
+          .mount("/hello", routes![world])         // To create new rocket server instance and mount the route.
 }
                 
